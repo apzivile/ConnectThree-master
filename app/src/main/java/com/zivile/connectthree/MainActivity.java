0e53da1e7348ed7477909ca6739d4d363f685d9e
zivile <zivile.apo@gmail.com>
2019-04-26 23:32:43 +0100

    added game logic

--- app/src/main/java/com/zivile/connectthree/MainActivity.java
+++ app/src/main/java/com/zivile/connectthree/MainActivity.java
@@ -4,9 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+//TODO FIX WINNING LAYOUT
+//TODO FIX who is winner
 public class MainActivity extends AppCompatActivity {
 
+    //yellow = 0 ,  red = 1
+
+    int activePlayer = 0;
+
+    //2 means unplayed
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +28,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
-        counter.setTranslationY(-1000f);
 
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.setImageResource(R.drawable.red);
-        counter.setImageResource(R.drawable.yellow);
-        counter.animate().translationYBy(1000f).setDuration(300);
+        if (gameState[tappedCounter] == 2) {
+            gameState[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).setDuration(300);
+
+            for (int[] winningPosition: winningPositions){
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]]!= 2 ){
+                   // System.out.println(gameState[winningPosition[0]]);
+
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
+                }
+            }
+        }
     }
-    
+
 }
